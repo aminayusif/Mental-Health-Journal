cefb776d33f273601d76878db860885c2c63c651
Amina Yusif <geekamie51@gmail.com>
2025-09-25 16:46:54 +0000

    Add update and delete endpoints to complete CRUD for journal entries

--- backend/journal/views.py
+++ backend/journal/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets, permissions
 from .models import JournalEntry
 from .serializers import JournalEntrySerializer
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
 
 
 # Removed unused JournalEntryList and JournalEntryDetail classes
@@ -19,6 +21,14 @@ class JournalEntryViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         # Automatically assign user when creating a journal entry
         serializer.save(user=self.request.user)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(
+            {"detail": "Journal entry deleted successfully"},
+            status=status.HTTP_200_OK
+        )
 
 class JournalEntryDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = JournalEntrySerializer
